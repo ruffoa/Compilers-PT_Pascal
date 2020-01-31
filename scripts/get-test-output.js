df474ef1684edb6368ace72566ad2ba2ebacc53a
ruffoa <ruffo.alex@gmail.com>
2020-01-30 21:30:42 -0500

    Make script a tad more robust

--- scripts/get-test-output.js
+++ scripts/get-test-output.js
@@ -9,10 +9,9 @@ const folderPath = path.join(__dirname, `../TestSuite/${segment}/`);
 const ptHomePath = path.join(__dirname, `../pt/`);
 
 async function loopTestDirectories() {
-    const getDirectories = a =>
-    readdirSync(folderPath, { withFileTypes: true })
+    const getDirectories = fs.readdirSync(folderPath, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+    .map(dirent => dirent.name);
 
     for (const dir of getDirectories) {
         await findAllFilesInDir(dir);
@@ -20,19 +19,19 @@ async function loopTestDirectories() {
 }
 
 async function findAllFilesInDir(dir) {
-    fs.readdirSync(dir).forEach(file => {
-        console.log(file);
+    fs.readdirSync(folderPath + dir).forEach(async file => {
         
         if (file.endsWith('.pt')) {
+            console.log(file);
             const res = await runFile(file);
-            writeResults(res, file);
+            writeResults(res, file, dir);
         }
     });          
 }
 
 async function runFile(file) {
-    const output = await exec(`ssltrace "ptc -o1 -t1 -L ${ptHomePath}lib/pt ${file}" ${ptHomePath}lib/pt/ scan .def -e`);
-
+    // const output = await exec(`ssltrace "ptc -o1 -t1 -L ${ptHomePath}lib/pt ${file}" ${ptHomePath}lib/pt/ scan .def -e`);
+    const output = await exec(`echo "HELOO"`);
     if (output.stderr) {
         console.error("ERROR IN FILE " + file + ": ", output.stderr);
     }
@@ -40,8 +39,10 @@ async function runFile(file) {
     return output.stderr || output.stdout;
 }
 
-function writeResults(content, file) {
-    fs.writeFileSync(`${file.substr(0, file.indexOf('.pt') - 3)}-output.txt`, '');
+function writeResults(content, file, dir) {
+    console.log("Writing to " + `${folderPath}${dir}/${file.substr(0, file.indexOf('.pt'))}-output.txt`);
+
+    fs.writeFileSync(`${folderPath}${dir}/${file.substr(0, file.indexOf('.pt') - 3)}-output.txt`, '');
 }
 
 loopTestDirectories();
