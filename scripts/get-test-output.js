41e5089dc59de1f0078b60ad85bca59fe9ef10f9
ruffoa <ruffo.alex@gmail.com>
2020-01-30 21:22:44 -0500

    Update testing output script

--- scripts/get-test-output.js
+++ scripts/get-test-output.js
@@ -6,20 +6,42 @@ var path = require('path');
 
 const segment = "Scanner"
 const folderPath = path.join(__dirname, `../TestSuite/${segment}/`);
+const ptHomePath = path.join(__dirname, `../pt/`);
 
-function loopTestDirectories() {
+async function loopTestDirectories() {
     const getDirectories = a =>
     readdirSync(folderPath, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
     for (const dir of getDirectories) {
-        
+        await findAllFilesInDir(dir);
     }
 }
 
-module.exports.getGitUser = async function getGitUser () {
-  const name = await exec('git config --global user.name')
-  const email = await exec('git config --global user.email')
-  return { name, email }
-};
+async function findAllFilesInDir(dir) {
+    fs.readdirSync(dir).forEach(file => {
+        console.log(file);
+        
+        if (file.endsWith('.pt')) {
+            const res = await runFile(file);
+            writeResults(res, file);
+        }
+    });          
+}
+
+async function runFile(file) {
+    const output = await exec(`ssltrace "ptc -o1 -t1 -L ${ptHomePath}lib/pt ${file}" ${ptHomePath}lib/pt/ scan .def -e`);
+
+    if (output.stderr) {
+        console.error("ERROR IN FILE " + file + ": ", output.stderr);
+    }
+
+    return output.stderr || output.stdout;
+}
+
+function writeResults(content, file) {
+    fs.writeFileSync(`${file.substr(0, file.indexOf('.pt') - 3)}-output.txt`, '');
+}
+
+loopTestDirectories();
