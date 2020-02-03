a3ec19135dbba1146d4e1805203461801ec5b9f2
Alex Ruffo <ruffo.alex@gmail.com>
2020-02-02 22:30:04 -0500

    Add in run all tests script (#13)
    * Add in run all tests `bash` script
    * Update README with info on how to run tests, and see existing outputs

---
+++ TestSuite/run-all-tests.sh
@@ -0,0 +1,28 @@
+# This script will run the tests in all of the directories and print the output to the terminal.
+# If easier to read, all of the *-output.txt files within the directory are actually the results of the test
+# runs we did as a group.  These files are auto-generated using a similar script to this one (but in Node 'cause it's better :P)
+
+# By the CMPE458 Dream Team - Alex Ruffo, David Findlay
+
+DIR=""
+
+if [ "$1" != "" ]; then
+    if [ "$1" == "scanner" ] || [ "$1" == "Scanner" ]; then 
+        DIR="Scanner"
+    else
+        echo "Sorry, directory does not exist, or tests have not yet been implemented yet"
+    fi
+
+    if [ "$DIR" != "" ]; then
+        for file in $DIR/*/*.pt
+        do
+            echo -e "\n----------------------------\nRunning tests for $file" 
+            ssltrace "ptc -o1 -t1 -L ../pt/lib/pt $file" ../pt/lib/pt/scan.def -e 
+            echo -e "----------------------------\n"
+        done
+    fi
+
+else
+    echo "Please enter the name of the section you want to run tests against (eg. 'Scanner'). This *should* match the directory name"
+fi
+
