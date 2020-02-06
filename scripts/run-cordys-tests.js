76c686d2d78df73a0f4976989c9f27b900c3d279
ruffoa <ruffo.alex@gmail.com>
2020-02-05 21:42:31 -0500

    Run file in alphabetical order, so that the output should in theory be consistant!

--- scripts/run-cordys-tests.js
+++ scripts/run-cordys-tests.js
@@ -20,7 +20,7 @@ const outputMap = {
 const stream = fs.createWriteStream(folderPath + "/combinedOutput.txt", {flags:'a'});
 
 async function findAllFilesInDir() {
-    fs.readdirSync(folderPath).forEach(async file => {
+    fs.readdirSync(folderPath).sort((a,b) => a < b).forEach(async file => {
         
         if (file.endsWith('.pt')) {
             console.log(file);
