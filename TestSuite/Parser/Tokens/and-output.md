e8b20da149ad4597022d9a656028b0ed8593e689
Alex Ruffo <ruffo.alex@gmail.com>
2020-02-06 22:24:56 -0500

    Update / Add new SSL definitions for the parser (#20)
    Changes the bare minimum to get this to compile, so there are a lot of commented out things and ToDo's
    
    This also adds in the CI for the parser test suite

---
+++ TestSuite/Parser/Tokens/and-output.md
@@ -0,0 +1,20 @@
+
+Test output is: 
+-------------------------
+```
+ .sProgram
+ .sIdentifier
+ .sParmEnd
+
+```
+------------------------
+
+File diff
+-------------------------
+```diff
+-.sProgram !== .pInteger on line 0 of and.pt
+- .sIdentifier !== .pDoubleAmpersand on line 1 of and.pt
+- .sParmEnd !== .pInteger on line 2 of and.pt
+
+```
+end file
--- scripts/get-test-output.js
+++ scripts/get-test-output.js
@@ -6,15 +6,22 @@ var path = require('path');
 
 const core = require('@actions/core');
 
-const segment = "Scanner"
+const args = (process.argv && process.argv.length >= 2 && process.argv.slice(2)) || [];
+
+const segment = (args && args[0]) || "Scanner";
 const folderPath = path.join(__dirname, `../TestSuite/${segment}/`);
 const relativeFolderPath = `../TestSuite/${segment}/`;
 const ptHomePath = path.join(__dirname, `../pt/`);
 
+const getSegment = {
+    'Scanner': '-o1 -t1',
+    'Parser': '-o2 -t2'
+};
+
 async function loopTestDirectories() {
     const getDirectories = fs.readdirSync(folderPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
 
     for (const dir of getDirectories) {
         await findAllFilesInDir(dir);
@@ -23,20 +30,25 @@ async function loopTestDirectories() {
 
 async function findAllFilesInDir(dir) {
     fs.readdirSync(folderPath + dir).forEach(async file => {
-        
+
         if (file.endsWith('.pt')) {
+            core.startGroup(`${dir} - ${file}`);
+
             console.log(file);
             const res = await runFile(file, dir);
-            writeResults(res, file, dir);
+            const fileDiff = compareResults(res, file, dir);
+            writeResults(fileDiff, file, dir);
+
+            core.endGroup();
         }
-    });          
+    });
 }
 
 async function runFile(file, dir) {
     try {
-        const output = await exec(`ssltrace "ptc -o1 -t1 -L ../pt/lib/pt ${relativeFolderPath}${dir}/${file}" ../pt/lib/pt/scan.def -e`);
+        const output = await exec(`ssltrace "ptc ${getSegment[segment]} -L ../pt/lib/pt ${relativeFolderPath}${dir}/${file}" ../pt/lib/pt/parser.def -e`);
         // const output = await exec(`echo "HELOO"`);
-        console.log(output.stdout, output.stderr || output.stdout);
+        // console.log(output.stdout, output.stderr || output.stdout);
 
         let isRealError = true;
 
@@ -47,19 +59,89 @@ async function runFile(file, dir) {
                 console.error("ERROR IN FILE " + file + ": ", output.stderr);
             }
         }
-
         return output.stderr && isRealError || output.stdout;
-        
+
     } catch (e) {
         console.error("Bash command failed, aborting! ", e);
         core.setFailed("Bash command failed, aborting" + e.message);
     }
 }
 
-function writeResults(content, file, dir) {
-    console.log("Writing to " + `${folderPath}${dir}/${file.substr(0, file.indexOf('.pt'))}-output.txt` + '\n--------------------------------\n');
+function compareResults(content, file, dir) {
+    let output = "";
 
-    fs.writeFileSync(`${folderPath}${dir}/${file.substr(0, file.indexOf('.pt'))}-output.txt`, content);
+    console.log(`\n--------------------------------\nReading file ${relativeFolderPath}${dir}/${file} from ${dir}`);
+    output += "\n";
+    
+    if (content)
+        output += `Test output is: \n-------------------------\n\`\`\`\n${content}\n\`\`\`\n------------------------\n`;
+
+    try {
+        const expectedResultFile = fs.readFileSync(`${relativeFolderPath}${dir}/${file}-e.txt`, 'utf-8');
+        // console.log(results, content)
+
+        if (!expectedResultFile || !content) {
+            let eFile = `the local test ouput (results from ${file})`;
+
+            if (!expectedResultFile) {
+                eFile = "Expected results"
+            }
+
+            console.log("No ", eFile);
+            // core.setFailed("Error, could not read " + eFile);
+            output += "Error, could not read " + eFile + '\n';
+            output += '\n```';
+
+            return output;
+        }
+
+        let expectedOutput = expectedResultFile.trim().split('\n');
+        if (expectedOutput[0].indexOf("PT Pascal v4.2 (c) 2019 Queen's University, (c) 1980 University of Toronto") >= 0) {
+            expectedOutput.splice(0, 1);
+        }
+
+        const testOutput = content.trim().split('\n');
+
+        if (expectedOutput.length !== testOutput.length) {
+            console.error("Lengths do not match!  Something went wrong in ", file);
+            console.error(`Output is: \n-------------------------\n${content}\n------------------------`);
+            // core.setFailed("Lengths do not match!  Something went wrong in " + file);
+
+            output += `Expected output length does not match!  Something went wrong in \`${file}\`\nShowing as much of the diff as possible...\n`;
+            // output += `Output is: \n-------------------------\n${content}\n------------------------\n`;
+
+            // return output;
+        }
+
+        output += "\nFile diff\n-------------------------" + '\n```diff\n';
+
+        const smallerOutput = testOutput.length < expectedOutput.length ? testOutput.length : expectedOutput.length;
+
+        for (var i = 0; i < smallerOutput; i++) {
+            // console.log(expectedOutput[i], testOutput[i]);
+
+            if (testOutput[i] !== expectedOutput[i]) {
+                console.error(`${testOutput[i]} !== ${expectedOutput[i]} on line ${i} of ${file}`);
+                // core.setFailed(`${testOutput[i]} !== ${expectedOutput[i]} on line ${i} of ${file}`);
+                    
+                output += `-${testOutput[i]} !== ${expectedOutput[i]} on line ${i} of ${file}\n`;
+            }
+        }
+    } catch (e) {
+        console.log(`No expected result file found (it must be called ${file}-e.txt)`);
+        output += "```diff\nReading file " + file + "\n";
+    }
+
+    output += '\n```';
+    output += "\nend file\n";
+
+    return output;
+}
+
+function writeResults(content, file, dir) {
+    console.log("Writing to " + `${folderPath}${dir}/${file.substr(0, file.indexOf('.pt'))}-output.md` + '\n--------------------------------\n');
+
+    fs.writeFileSync(`${folderPath}${dir}/${file.substr(0, file.indexOf('.pt'))}-output.md`, content);
 }
 
 loopTestDirectories();
